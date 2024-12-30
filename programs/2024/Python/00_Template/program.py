8ce1c1bcf465b709ade3337b4dfc9efe2a4cbedc
Céleste VANDAMME <Slaker@outlook.fr>
2024-12-30 15:52:22 +0100

    Update template program.py
    Update: improved the template file to integrate proper file reading

--- programs/2024/Python/00_Template/program.py
+++ programs/2024/Python/00_Template/program.py
@@ -22,15 +22,40 @@
 
 # --- A. GLOBAL PARAMETERS
 
+INPUT_FILE_DIR = "programs/2024/Python/xxx/xxx.txt" # TODO: replace text directory here
 
 
 # --- B. FUNCTIONS
 
+def inputReading( ) -> list :
+    """
+    Reads the input from the file specified by `INPUT_FILE_DIR` and returns
+    each line as an element in a list.
+
+    Returns:
+        list: A list of strings, where each string is a line from the input file.
+    """
+        
+    rawInputLines = []
+    
+    with open(INPUT_FILE_DIR, "r") as input_file:
+    
+        for line in input_file:
+            rawInputLines.append( line )
+        
+    return rawInputLines
 
 
 # --- C. main.py
 
 def main():
+    
+    # -- 1. Input read    
+    inputLines = inputReading()
+    
+    
+    # -- 2. (...)
+    
     
     # Closing "main()"
     return
